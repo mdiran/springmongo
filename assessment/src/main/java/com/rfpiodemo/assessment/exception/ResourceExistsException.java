02eea2004e9501f02b3e9319bf2b5710d8db335c
mdiran <123882144+mdiran@users.noreply.github.com>
2023-01-29 20:14:07 +0530

    Delete assessment directory

--- assessment/src/main/java/com/rfpiodemo/assessment/exception/ResourceExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.rfpiodemo.assessment.exception;
-
-public class ResourceExistsException extends RuntimeException{
-
-	public ResourceExistsException(String message) {
-		super(message);
-	}
-
-}
--- assessment/src/main/java/com/rfpiodemo/assessment/exception/UserResourceException.java
+++
@@ -1,9 +0,0 @@
-package com.rfpiodemo.assessment.exception;
-
-public class UserResourceException extends RuntimeException{
-
-	public UserResourceException(String message) {
-		super(message);
-	}
-
-}
